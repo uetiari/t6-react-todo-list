2aec10fef7d5b163a127d4bed5d82ffb9fb2928a
uetiari <arianeueti@gmail.com>
2018-11-21 13:36:23 -0200

    Adicionado função para pegar conteúdo escrito no Postit

--- my-app/src/components/postit/postit.js
+++ my-app/src/components/postit/postit.js
@@ -7,6 +7,9 @@ class Postit extends React.Component {
     constructor(props){
         super(props)
         this.state = {
+            id: 0,
+            title: '',
+            text: '', 
             editing : false
         }
     }
@@ -17,20 +20,56 @@ class Postit extends React.Component {
             })
         }
 
+        handlePostitRemove = () => {
+            console.log('handlePostitRemove =D')
+        }
+
+        handlePostitSubmit = (e) => {
+            e.preventDefault()
+    
+            const postit = {
+                title : this.state.title,
+                text : this.state.text 
+                }
+
+        setTitle = (e) => {
+            const inputTitle = e.target.value
+            this.setState({
+                title : inputTitle
+            }
+            )
+        }
+
         render(){
             return (
                 <div className='postit' onClick={this.handlePostitClick}>
-                    <Form>
+                    <Form onSubmit={this.handlePostitSubmit}>
                         {this.state.editing && (
-                            <button className='postit__button-remove'>
+                            <button onClick={this.handlePostitRemove} className='postit__button-remove'>
                             x
                             </button>
                         )
                         }
 
-                        <input type='text' className='postit__title' placeholder='Título' />
-                        <textarea className='postit__text' placeholder='Digite o texto' name='text' />
-                        <button className='postit__button-completed'> Concluído </button>
+                        <input 
+                        type='text' 
+                        className='postit__title' 
+                        placeholder='Título' 
+                        value={this.state.title}
+                        onChange={this.setTitle}
+                        />
+
+                        <textarea 
+                        className='postit__text' 
+                        placeholder='Digite o texto' 
+                        name='text' 
+                        value={this.state.text}
+                        />
+                        
+                        {this.state.editing && (
+                            <button className='postit__button-completed'> Concluído </button>
+                        )
+                        }
                     </Form>
                 </div>
             )

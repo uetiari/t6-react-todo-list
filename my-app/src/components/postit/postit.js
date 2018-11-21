f3579cf54892c7749bcad654851a5b5a118c4c9b
uetiari <arianeueti@gmail.com>
2018-11-21 13:56:12 -0200

    Adicionado função para pegar conteúdo escrito no corpo do Postit

--- my-app/src/components/postit/postit.js
+++ my-app/src/components/postit/postit.js
@@ -4,68 +4,78 @@ import Form from '../form'
 
 
 class Postit extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             id: 0,
             title: '',
-            text: '', 
-            editing : false
+            text: '',
+            editing: false
         }
     }
-        
-        handlePostitClick = () => {
-            this.setState({
-                editing: true
-            })
+
+    handlePostitClick = () => {
+        this.setState({
+            editing: true
+        })
+    }
+
+    handlePostitRemove = () => {
+        console.log('handlePostitRemove =D')
+    }
+
+    handlePostitSubmit = (e) => {
+        e.preventDefault()
+
+        const postit = {
+            title: this.state.title,
+            text: this.state.text
         }
+    }
 
-        handlePostitRemove = () => {
-            console.log('handlePostitRemove =D')
+    setTitle = (e) => {
+        const inputTitle = e.target.value
+        this.setState({
+            title: inputTitle
         }
+        )
+    }
 
-        handlePostitSubmit = (e) => {
-            e.preventDefault()
-    
-            const postit = {
-                title : this.state.title,
-                text : this.state.text 
-                }
-
-        setTitle = (e) => {
-            const inputTitle = e.target.value
-            this.setState({
-                title : inputTitle
-            }
-            )
+    setText = (e) => {
+        const inputText = e.target.value
+        this.setState({
+            text: inputText
         }
+        )
+    }
 
-        render(){
+
+        render() {
             return (
                 <div className='postit' onClick={this.handlePostitClick}>
                     <Form onSubmit={this.handlePostitSubmit}>
                         {this.state.editing && (
                             <button onClick={this.handlePostitRemove} className='postit__button-remove'>
-                            x
+                                x
                             </button>
-                        )
-                        }
+                        )}
 
-                        <input 
-                        type='text' 
-                        className='postit__title' 
-                        placeholder='Título' 
-                        value={this.state.title}
-                        onChange={this.setTitle}
+                        <input
+                            type='text'
+                            className='postit__title'
+                            placeholder='Título'
+                            value={this.state.title}
+                            onChange={this.setTitle}
                         />
 
-                        <textarea 
-                        className='postit__text' 
-                        placeholder='Digite o texto' 
-                        name='text' 
-                        value={this.state.text}
+                        <textarea
+                            className='postit__text'
+                            placeholder='Digite o texto'
+                            name='text'
+                            value={this.state.text}
+                            onChange={this.setText}
                         />
-                        
+
                         {this.state.editing && (
                             <button className='postit__button-completed'> Concluído </button>
                         )
@@ -76,4 +86,4 @@ class Postit extends React.Component {
         }
 }
 
-export default Postit
+    export default Postit
